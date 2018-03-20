2de220ed32d44a447001aff5a88f82eda78edf12
eduardosmatheus <matheus-edu@outlook.com>
2018-03-19 22:51:50 -0300

    Refactor code to satisfy Udacity guidelines.

--- program.py
+++ program.py
@@ -1,37 +1,47 @@
+#!/usr/bin/env python3
 import psycopg2
 
+
 def get_connection():
-    return psycopg2.connect("dbname=news")
+    try:
+        db = psycopg2.connect("dbname=news")
+        return db
+    except psycopg2.Error:
+        print("Failed to connect!!!!")
+
 
 def get_most_popular_articles_all_time():
     conn = get_connection()
     cur = conn.cursor()
     cur.execute(
-        "select articles.title, log.path, count(*) as views "
+        "select articles.title, count(*) as views "
         "from articles "
-        "inner join log on log.path like concat('%', articles.slug, '%') "
-        "where log.status like '%200%' "
-        "group by articles.title, log.path "
+        "inner join log on log.path = concat('/article/', articles.slug) "
+        "group by articles.title "
         "order by views desc limit 3"
     )
     result = cur.fetchall()
     conn.close()
     return result
 
+
 def get_most_popular_authors():
     conn = get_connection()
     cur = conn.cursor()
     cur.execute(
-        "select authors.name, count(*) as views from articles inner "
-        "join authors on articles.author = authors.id inner join log "
-        "on log.path like concat('%', articles.slug, '%') where "
-        "log.status like '%200%' group by authors.name order by views desc"
+        "select authors.name, count(*) as views "
+        "from articles "
+        "inner join authors on articles.author = authors.id "
+        "inner join log on log.path = concat('/article/', articles.slug) "
+        "group by authors.name "
+        "order by views desc"
     )
     result = cur.fetchall()
     conn.close()
     return result
 
-def get_one_percent_errors():
+
+def get_errors():
     conn = get_connection()
     cur = conn.cursor()
     cur.execute(
@@ -47,31 +57,45 @@ def get_one_percent_errors():
     conn.close()
     return result
 
-def print_error_results(query_results):
-    print (query_results[1])
-    for results in query_results[0]:
-        print ("\t", results[0], "-", str(results[1]) + "% errors")
 
-if __name__ == "__main__":
-    
+def print_most_popular_articles():
     most_popular_articles = get_most_popular_articles_all_time()
     print("1. Quais são os três artigos mais populares de todos os tempos?")
     for index, article in enumerate(most_popular_articles):
         title, views = article[0], article[2]
-        print("\t", index + 1, "- {title} - {views} views".format(title=title, views=views))
-    print("\n")
-    
-    print("2. Quem são os autores de artigos mais populares de todos os tempos?")
+        print(
+            "\t", index + 1,
+            "- {title} - {views} views"
+            .format(title=title, views=views))
+
+
+def print_most_popular_authors():
+    print("2. Quem são os autores mais populares de todos os tempos?")
     most_popular_authors = get_most_popular_authors()
     for index, author in enumerate(most_popular_authors):
         author_name, views = author[0], author[1]
-        print("\t", index + 1, "- {name} - {views} views".format(name=author_name, views=views))
-    
-    print("\n")
+        print(
+            "\t", index + 1,
+            "- {name} - {views} views"
+            .format(name=author_name, views=views))
+
+
+def print_errors():
     print("3. Em quais dias mais de 1% das requisições resultaram em erros?")
-    errors = get_one_percent_errors()
+    errors = get_errors()
     for index, error in enumerate(errors):
-        print("\t", index + 1, "- {day} - {average}".format(day=error[0], average=error[1]))
+        print(
+            "\t", index + 1,
+            "- {day} - {average}"
+            .format(day=error[0], average=error[1]))
 
 
+if __name__ == "__main__":
 
+    print_most_popular_articles()
+    print("\n")
+
+    print_most_popular_authors()
+    print("\n")
+
+    print_errors()
